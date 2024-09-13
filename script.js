453cdd280f6f4657d6d260d98aefe568827911e4
Amiya Ranjan Das <97892336+AmiyaRanjanDas@users.noreply.github.com>
2024-09-13 20:38:18 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// ===============================Hero================================
+TweenMax.staggerFrom(".line1", 3, {
+    opacity: 0,
+    x: -100,
+    ease: Expo.easeInOut,
+    delay: 0
+}, 0.09);
+TweenMax.staggerFrom(".line2", 3, {
+    opacity: 0,
+    x: 100,
+    ease: Expo.easeInOut,
+    delay: 0
+}, 0.09);
+TweenMax.staggerFrom(".round1", 1, {
+    opacity: 0,
+    scale: 0.5,
+    ease: Expo.easeOut,
+    delay: 1.2
+}, 0.09);
+TweenMax.staggerFrom(".round2", 1, {
+    opacity: 0,
+    scale: 0.5,
+    ease: Expo.easeOut,
+    delay: 1.5
+}, 0.09);
+TweenMax.staggerFrom(".round3", 1, {
+    opacity: 0,
+    scale: 0.5,
+    ease: Expo.easeOut,
+    delay: 1.8
+}, 0.09);
+TweenMax.staggerFrom(".main_img", 1.5, {
+    opacity: 0,
+    y: 100,
+    ease: Expo.easeInOut,
+    delay: 2
+}, 0.09);
+
+
+// ===============================About================================
+
